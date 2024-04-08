ec5b294e065031db932c4f4cb413067ebc8cc3e2
OsamuMurataNeec <murataosa@gmail.com>
2024-04-08 17:42:25 +0900

    1-2 再帰関数

--- main.cpp
+++ main.cpp
@@ -1,28 +1,22 @@
 #include <stdio.h>
 
-template <typename T>
-T sub(T a, T b) {
-	return a + b;
+int Recursive(int n,int kyuyo) {
+
+	if (n <= 1) {
+		return 0;
+	}
+
+	printf("階乗%d時間目:%d\n",11-n,kyuyo);
+	n--;
+	return (kyuyo + Recursive(n,kyuyo*2-50));
 }
 
-template <>
-float sub<float>(float a, float b) {
+int main() {
+	int n = 10; // 階乗する値
+	int result; // 計算結果
 
-	if (a <= b)
-		return a;
-	else
-		return b;
-}
+	printf("通常で10時間働くと%d\n",1072*10);
+	result = Recursive(n,100);
 
-char sub(char a, char b) {
-	return puts("charは使用不可");
-}
-
-int main(void) {
-	printf("%d\n", sub<int>(114, 514));       // 628    関数テンプレートを利用
-	printf("%f\n", sub<float>(11.4f, 51.4f)); // -40.0  特殊化(オーバーライド)を利用
-	printf("%f\n", sub<double>(11.4, 51.4));        // 585.96 多重定義(オーバーロード)を利用
-	printf("%c\n", sub<char>('a','b'));        // 585.96 多重定義(オーバーロード)を利用
-
-	return 0;
+	return (0);
 }
