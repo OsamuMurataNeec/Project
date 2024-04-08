60629affb16eaa85a841b91d60151d3c7ab72d6d
OsamuMurataNeec <murataosa@gmail.com>
2024-04-08 17:17:25 +0900

    1-1オーバーライドとオーバーロード

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
-int main()
-{
-	printf("test");
+template <typename T>
+T sub(T a, T b) {
+	return a + b;
+}
+
+template <>
+float sub<float>(float a, float b) {
+
+	if (a <= b)
+		return a;
+	else
+		return b;
+}
+
+char sub(char a, char b) {
+	return puts("charは使用不可");
+}
+
+int main(void) {
+	printf("%d\n", sub<int>(114, 514));       // 628    関数テンプレートを利用
+	printf("%f\n", sub<float>(11.4f, 51.4f)); // -40.0  特殊化(オーバーライド)を利用
+	printf("%f\n", sub<double>(11.4, 51.4));        // 585.96 多重定義(オーバーロード)を利用
+	printf("%c\n", sub<char>('a','b'));        // 585.96 多重定義(オーバーロード)を利用
+
 	return 0;
 }
